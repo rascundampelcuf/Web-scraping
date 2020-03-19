9780dd2d942ddf806ef04ca4d01077c73fe5e952
rascundampelcuf <g.izquierdoalcaraz@gmail.com>
2020-03-19 14:15:21 +0100

    Update scraper.py
    :sparkles: Created download_html method

--- src/scraper.py
+++ src/scraper.py
@@ -1,5 +1,14 @@
 
+import requests
+from bs4 import BeautifulSoup
+
+
 class Scraper():
       
       def __init__(self):
             self.url = 'https://www.casadellibro.com/libros/literatura/121000000'
+          
+      def __download_html(self, url):
+            html = requests.get(url)
+            soup = BeautifulSoup(html.content, 'html.parser')
+            return soup
