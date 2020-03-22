d3f7fb745d90ebb6ac89ad0e2b7aabf4a5c23c7c
rascundampelcuf <g.izquierdoalcaraz@gmail.com>
2020-03-22 16:40:33 +0100

    :art: Changed indent to 4 spaces

--- src/scraper.py
+++ src/scraper.py
@@ -5,18 +5,18 @@ from bs4 import BeautifulSoup
 
 class Scraper():
       
-      def __init__(self):
-            self.url = 'https://www.casadellibro.com/libros/literatura/121000000'
+    def __init__(self):
+        self.url = 'https://www.casadellibro.com/libros/literatura/121000000'
           
-      def __download_html(self, url):
-            html = requests.get(url)
-            soup = BeautifulSoup(html.content, 'html.parser')
-            return soup
+    def __download_html(self, url):
+        html = requests.get(url)
+        soup = BeautifulSoup(html.content, 'html.parser')
+        return soup
     
-      def scrape(self):
-            page = self.__download_html(self.url)
-            
-            print(page.prettify())
+    def scrape(self):
+        page = self.__download_html(self.url)
+        
+        print(page.prettify())
             
 
 Scraper().scrape()
