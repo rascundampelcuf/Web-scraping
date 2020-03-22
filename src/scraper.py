337a4349a7a2c9cb936663f44808a077006da7da
rascundampelcuf <g.izquierdoalcaraz@gmail.com>
2020-03-22 19:17:51 +0100

    :sparkles: Created demo get_titles method

--- src/scraper.py
+++ src/scraper.py
@@ -1,6 +1,7 @@
 
 import requests
 from bs4 import BeautifulSoup
+from book import Book
 
 
 class Scraper():
@@ -13,10 +14,27 @@ class Scraper():
         soup = BeautifulSoup(html.content, 'html.parser')
         return soup
     
+    def __get_titles(self, page):
+        books = []
+        divs = page.findAll('div')
+        for div in divs:
+            book = Book()
+            try:
+                if div['class'] == ['product__info']:
+                    book.set_title(div.a.string.strip())
+                    books.append(book)
+            except Exception:
+                pass
+            
+        return books
+    
     def scrape(self):
         page = self.__download_html(self.url)
+        titles = self.__get_titles(page)
         
-        print(page.prettify())
+        for book in titles:
+            print(book)
+        # print(page.prettify())
             
 
 Scraper().scrape()
